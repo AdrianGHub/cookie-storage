d11247cea084fb2a5a25ea66a99dd5cdc8712c60
Adrian <adrian_zinko@op.pl>
2020-03-30 19:19:45 +0200

    feat: query DOM elements

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+const shureButton = document.querySelector('.button--first');
+const nopeButton = document.querySelector('.button--second');
+const divs = document.querySelectorAll('div');
