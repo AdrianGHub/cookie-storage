87568412e09357bf838518453c8779c4c5b8eb87
Adrian <adrian_zinko@op.pl>
2020-03-30 19:37:44 +0200

    feat: implement buttons functionality on click event

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ import './style.css';
 const shureButton = document.querySelector('.button--first');
 const nopeButton = document.querySelector('.button--second');
 const divs = document.querySelectorAll('div');
+const blackHole = document.querySelector("img");
+
+function checkCookie() {
+    const cookie = localStorage.getItem('cookie');
+    cookie ? divs.forEach(div => div.style.display = 'none') : null;
+  }
+  
+  shureButton.addEventListener('click', e => {
+    e.preventDefault();
+  
+    localStorage.setItem('cookie', true);
+  
+
+    blackHole.classList.add("grow");
+
+    divs.forEach(div => {
+        div.style.opacity = '0';
+        div.style.transition = '5s';
+        div.classList.add('absorp');
+      });
+
+    
+  })
+
+
+  
+  nopeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+  
+    divs.forEach(div => {
+      div.style.opacity = '0';
+      div.style.transition = '.7s';
+    });
+  })
+  
+  checkCookie();
